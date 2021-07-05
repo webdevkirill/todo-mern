3195b3489ff600fe23725692c58bdd433c36638d
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-07-05 21:15:16 +0300

    Добавляет валидацию данных

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,24 +1,42 @@
 const { Router } = require('express');
 const User = require('../models/User');
+const { check, validationResult } = require('express-validator');
 
 const router = new Router();
 
-router.post('/registration', async (req, res) => {
-	try {
-		const { email, password } = req.body;
-		const candidate = await User.findOne({ email: email });
+router.post(
+	'/registration',
+	[
+		check('email', 'Некорректный email').isEmail(),
+		check('password', 'Некорректный пароль').isLength({ min: 6 }),
+	],
+	async (req, res) => {
+		try {
+			const errors = validationResult(req);
 
-		if (candidate) {
-			return res.status(300).json({ message: 'Такой email уже занят' });
+			if (!errors.isEmpty()) {
+				return res
+					.status(400)
+					.json({ errors: errors.array(), message: errors[0] });
+			}
+
+			const { email, password } = req.body;
+			const candidate = await User.findOne({ email: email });
+
+			if (candidate) {
+				return res
+					.status(300)
+					.json({ message: 'Такой email уже занят' });
+			}
+
+			const user = new User({ email, password });
+			await user.save();
+
+			res.status(201).json({ message: 'Пользователь создан' });
+		} catch (err) {
+			console.error(err);
 		}
-
-		const user = new User({ email, password });
-		await user.save();
-
-		res.status(201).json({ message: 'Пользователь создан' });
-	} catch (err) {
-		console.error(err);
 	}
-});
+);
 
 module.exports = router;
