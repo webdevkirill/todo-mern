e4c3b19e27b5006fdbed9a4ad4dae0f96b2579d9
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-07-05 21:33:12 +0300

    Добавляет вход на сервере

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const dotenv = require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const router = new Router();
@@ -36,6 +38,49 @@ router.post(
 			await user.save();
 
 			res.status(201).json({ message: 'Пользователь создан' });
+		} catch (err) {
+			console.error(err);
+		}
+	}
+);
+
+router.post(
+	'/login',
+	[
+		check('email', 'Некорректный email').isEmail(),
+		check('password', 'Некорректный пароль').exists(),
+	],
+	async (req, res) => {
+		try {
+			const errors = validationResult(req);
+
+			if (!errors.isEmpty()) {
+				return res
+					.status(400)
+					.json({ errors: errors.array(), message: errors[0] });
+			}
+
+			const { email, password } = req.body;
+
+			const user = await User.findOne({ email });
+
+			if (!user) {
+				return res
+					.status(400)
+					.json({ message: 'Такой email не найден' });
+			}
+
+			const isSamePass = bctypt.compare(password, user.password);
+
+			if (!isSamePass) {
+				return res.status(400).json({ message: 'Пароль неверный' });
+			}
+
+			const token = jwt.sign({ userId: user.id }, process.env.TOKEN_KEY, {
+				expiresIn: '1h',
+			});
+
+			res.json({ token, userId: user.id });
 		} catch (err) {
 			console.error(err);
 		}
