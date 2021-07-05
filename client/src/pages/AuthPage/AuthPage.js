c9b261fac5f1ec8ea95dd1ca81707420b153571d
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-07-05 20:40:58 +0300

    Добавляет роуты для авторизации на клиенте

--- client/src/pages/AuthPage/AuthPage.js
+++ client/src/pages/AuthPage/AuthPage.js
@@ -1,76 +1,95 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './authPage.sass';
 
 export default function AuthPage() {
 	return (
-		<>
-			<div className='container'>
-				<div className='auth-page'>
-					<h3>Вход</h3>
-					<form className='form form-login'>
-						<div className='row'>
-							<div className='input-field col s12'>
-								<input
-									type='email'
-									name='email'
-									id='login-email'
-									className='validate'
-								/>
-								<label htmlFor='login-email'>Email</label>
-							</div>
-							<div className='input-field col s12'>
-								<input
-									type='password'
-									name='password'
-									id='login-password'
-									className='validate'
-								/>
-								<label htmlFor='login-password'>Пароль</label>
-							</div>
-						</div>
-						<div className='row'>
-							<button className='wawes-effect wawes-light btn blue'>
-								Войти
-							</button>
-							<a href='/' className='btn-outline btn-reg'>
-								Нет аккаунта?
-							</a>
-						</div>
-					</form>
+		<BrowserRouter>
+			<Switch>
+				<div className='container'>
+					<div className='auth-page'>
+						<Route path='/login'>
+							<h3>Вход</h3>
+							<form className='form form-login'>
+								<div className='row'>
+									<div className='input-field col s12'>
+										<input
+											type='email'
+											name='email'
+											id='login-email'
+											className='validate'
+										/>
+										<label htmlFor='login-email'>
+											Email
+										</label>
+									</div>
+									<div className='input-field col s12'>
+										<input
+											type='password'
+											name='password'
+											id='login-password'
+											className='validate'
+										/>
+										<label htmlFor='login-password'>
+											Пароль
+										</label>
+									</div>
+								</div>
+								<div className='row'>
+									<button className='wawes-effect wawes-light btn blue'>
+										Войти
+									</button>
+									<Link
+										to='/registration'
+										className='btn-outline btn-reg'
+									>
+										Нет аккаунта?
+									</Link>
+								</div>
+							</form>
+						</Route>
 
-					<h3>Регистрация</h3>
-					<form className='form form-login'>
-						<div className='row'>
-							<div className='input-field col s12'>
-								<input
-									type='email'
-									name='email'
-									id='reg-email'
-									className='validate'
-								/>
-								<label htmlFor='reg-email'>Email</label>
-							</div>
-							<div className='input-field col s12'>
-								<input
-									type='password'
-									name='password'
-									id='reg-password'
-									className='validate'
-								/>
-								<label htmlFor='reg-password'>Пароль</label>
-							</div>
-						</div>
-						<div className='row'>
-							<button className='wawes-effect wawes-light btn blue'>
-								Регистрация
-							</button>
-							<a href='/' className='btn-outline btn-reg'>
-								Уже зарегистрированы?
-							</a>
-						</div>
-					</form>
+						<Route path='/registration'>
+							<h3>Регистрация</h3>
+							<form className='form form-login'>
+								<div className='row'>
+									<div className='input-field col s12'>
+										<input
+											type='email'
+											name='email'
+											id='reg-email'
+											className='validate'
+										/>
+										<label htmlFor='reg-email'>Email</label>
+									</div>
+									<div className='input-field col s12'>
+										<input
+											type='password'
+											name='password'
+											id='reg-password'
+											className='validate'
+										/>
+										<label htmlFor='reg-password'>
+											Пароль
+										</label>
+									</div>
+								</div>
+								<div className='row'>
+									<button className='wawes-effect wawes-light btn blue'>
+										Регистрация
+									</button>
+									<Link
+										to='/login'
+										className='btn-outline btn-reg'
+									>
+										Уже зарегистрированы?
+									</Link>
+								</div>
+							</form>
+						</Route>
+					</div>
 				</div>
-			</div>
-		</>
+			</Switch>
+		</BrowserRouter>
 	);
 }
