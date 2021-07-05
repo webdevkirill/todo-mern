b9c91cfcf99c8d5f1c2cd51834d4de2e7e63a205
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-07-05 20:47:52 +0300

    Добавляет обработку форм взода и регистрации

--- client/src/pages/AuthPage/AuthPage.js
+++ client/src/pages/AuthPage/AuthPage.js
@@ -1,94 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './authPage.sass';
 
 export default function AuthPage() {
+	const [form, setForm] = useState({
+		email: '',
+		password: '',
+	});
+
+	const inputChangeHandler = (e) => {
+		setForm({ ...form, [e.target.name]: e.target.value });
+		console.log(form);
+	};
+
 	return (
 		<BrowserRouter>
 			<Switch>
-				<div className='container'>
-					<div className='auth-page'>
-						<Route path='/login'>
-							<h3>Вход</h3>
-							<form className='form form-login'>
-								<div className='row'>
-									<div className='input-field col s12'>
-										<input
-											type='email'
-											name='email'
-											id='login-email'
-											className='validate'
-										/>
-										<label htmlFor='login-email'>
-											Email
-										</label>
+				<>
+					<div className='container'>
+						<div className='auth-page'>
+							<Route path='/login'>
+								<h3>Вход</h3>
+								<form className='form form-login'>
+									<div className='row'>
+										<div className='input-field col s12'>
+											<input
+												type='email'
+												name='email'
+												id='login-email'
+												className='validate'
+												onChange={inputChangeHandler}
+											/>
+											<label htmlFor='login-email'>
+												Email
+											</label>
+										</div>
+										<div className='input-field col s12'>
+											<input
+												type='password'
+												name='password'
+												id='login-password'
+												className='validate'
+												onChange={inputChangeHandler}
+											/>
+											<label htmlFor='login-password'>
+												Пароль
+											</label>
+										</div>
 									</div>
-									<div className='input-field col s12'>
-										<input
-											type='password'
-											name='password'
-											id='login-password'
-											className='validate'
-										/>
-										<label htmlFor='login-password'>
-											Пароль
-										</label>
+									<div className='row'>
+										<button className='wawes-effect wawes-light btn blue'>
+											Войти
+										</button>
+										<Link
+											to='/registration'
+											className='btn-outline btn-reg'
+										>
+											Нет аккаунта?
+										</Link>
 									</div>
-								</div>
-								<div className='row'>
-									<button className='wawes-effect wawes-light btn blue'>
-										Войти
-									</button>
-									<Link
-										to='/registration'
-										className='btn-outline btn-reg'
-									>
-										Нет аккаунта?
-									</Link>
-								</div>
-							</form>
-						</Route>
+								</form>
+							</Route>
 
-						<Route path='/registration'>
-							<h3>Регистрация</h3>
-							<form className='form form-login'>
-								<div className='row'>
-									<div className='input-field col s12'>
-										<input
-											type='email'
-											name='email'
-											id='reg-email'
-											className='validate'
-										/>
-										<label htmlFor='reg-email'>Email</label>
+							<Route path='/registration'>
+								<h3>Регистрация</h3>
+								<form className='form form-login'>
+									<div className='row'>
+										<div className='input-field col s12'>
+											<input
+												type='email'
+												name='email'
+												id='reg-email'
+												onChange={inputChangeHandler}
+												className='validate'
+											/>
+											<label htmlFor='reg-email'>
+												Email
+											</label>
+										</div>
+										<div className='input-field col s12'>
+											<input
+												type='password'
+												name='password'
+												id='reg-password'
+												onChange={inputChangeHandler}
+												className='validate'
+											/>
+											<label htmlFor='reg-password'>
+												Пароль
+											</label>
+										</div>
 									</div>
-									<div className='input-field col s12'>
-										<input
-											type='password'
-											name='password'
-											id='reg-password'
-											className='validate'
-										/>
-										<label htmlFor='reg-password'>
-											Пароль
-										</label>
+									<div className='row'>
+										<button className='wawes-effect wawes-light btn blue'>
+											Регистрация
+										</button>
+										<Link
+											to='/login'
+											className='btn-outline btn-reg'
+										>
+											Уже зарегистрированы?
+										</Link>
 									</div>
-								</div>
-								<div className='row'>
-									<button className='wawes-effect wawes-light btn blue'>
-										Регистрация
-									</button>
-									<Link
-										to='/login'
-										className='btn-outline btn-reg'
-									>
-										Уже зарегистрированы?
-									</Link>
-								</div>
-							</form>
-						</Route>
+								</form>
+							</Route>
+						</div>
 					</div>
-				</div>
+				</>
 			</Switch>
 		</BrowserRouter>
 	);
